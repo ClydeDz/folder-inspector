fd4056c23d48acdf99245d7379164417d60ccf14
Clyde D'Souza <clyd94@gmail.com>
2019-04-14 19:08:47 +1200

    Add xUnit setup

---
+++ FolderInspector.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace FolderInspector.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.True(true);
+        }
+    }
+}
